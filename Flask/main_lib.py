2f04a58385d06de80d0aa93533905fea65982929
chebupel01 <mironov.igor120.igor@gmail.com>
2024-02-12 18:02:32 +0300

    4 задание

--- Flask/main_lib.py
+++ Flask/main_lib.py
@@ -1,11 +1,26 @@
-from flask import Flask, url_for, request, render_template
+from flask import Flask, request, render_template, redirect
+from flask import url_for
+from pprint import pprint
+
+from flask_wtf import FlaskForm
+from wtforms import StringField, PasswordField, BooleanField, SubmitField
+from wtforms.validators import DataRequired
 
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'yandexlyceum_secret_key'
+
+class LoginForm(FlaskForm):
+    id_astr = StringField('id астронавта', validators=[DataRequired()])
+    password = PasswordField('пароль астронавта', validators=[DataRequired()])
+    id_kap = StringField('id капитана', validators=[DataRequired()])
+    password_kap = PasswordField('пароль капитана', validators=[DataRequired()])
+    submit = SubmitField('Доступ')
+
 
 
 @app.route('/<title>')
 @app.route('/index/<title>')
-def index(title):
+def home(title):
     return render_template('base.html', title=title)
 
 
@@ -14,24 +29,43 @@ def training(prof):
     return render_template('training.html', prof=prof)
 
 
-@app.route('/list_prof/<types>')
-def list_prof(types):
-    a = 1
-    proflist = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
-                'инженер по терраформированию', 'климатолог',
-                'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
-                'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
-                'штурман', 'пилот дронов']
-    return render_template('list_prof.html', types=types, proflist=proflist)
+@app.route('/list_prof/<flag>')
+def abc(flag):
+    nng = ['инженер-исследователь', 'пилот', 'строитель', 'экзобиолог', 'врач',
+           'инженер по терраформированию', 'климатолог',
+           'специалист по радиационной защите', 'астрогеолог', 'гляциолог',
+           'инженер жизнеобеспечения', 'метеоролог', 'оператор марсохода', 'киберинженер',
+           'штурман', 'пилот дронов']
+    return render_template('proof.html', prof=flag, proflist=nng)
 
 
 @app.route('/answer')
 @app.route('/auto_answer')
-def answers():
-    data = {'title': 'Анкета', 'surname': 'Васильев', 'name': 'Дима', 'education': 'Высшее', 'profession': 'Космонавт',
-            'sex':
-                'Мужской', 'motivation': 'Всегда мечтал покушать картошки с марса', 'ready': 'Готов'}
-    return render_template('auto_answer.html', **data)
+def answer():
+    anketa = {'title': 'Firsov',
+              'surname': 'Mask',
+              'name': 'Ilon',
+              'education': 'higher',
+              'profession': 'billionaire',
+              'sex': 'man',
+              'motivation': 'Mars',
+              'ready': 'yes'
+              }
+    return render_template('auto_answer.html', **anketa)
+
+@app.route('/armor', methods=['GET', 'POST'])
+def armor():
+    form = LoginForm()
+    if form.validate_on_submit():
+        return redirect('/success')
+    return render_template('armor.html', title='Аварийный доступ', form=form)
+
+
+@app.route('/success')
+def success():
+    return render_template('success.html')
+
+
 
 
 if __name__ == '__main__':
